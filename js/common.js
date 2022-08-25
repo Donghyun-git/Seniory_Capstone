f87f755d4a6041bb9d3b49a8da3f8cb2b97defed
donghyun-git <donghuyn1113@hanmail.net>
2022-08-26 03:07:54 +0900

    1

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$('#gnb-menu').click(function(){
+		if($(this).is('.on')) {
+			$(this).removeClass('on');
+			$('#gnb-list').slideUp('fast');
+		}else {
+			$(this).addClass('on');
+			$('#gnb-list').slideDown('fast');
+		}
+	});	
+});
+
+
+
