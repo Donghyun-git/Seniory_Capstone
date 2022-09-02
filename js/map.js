8047cad6c5f14aebced47dcb826981300f5b9656
donghyun-git <donghuyn1113@hanmail.net>
2022-09-02 14:03:46 +0900

    9

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 /* user location */
-var map = new naver.maps.Map('map', {
+
+var mapOptions = new naver.maps.Map('map', {
     center: new naver.maps.LatLng(37.3595704, 127.105399),
     zoom: 10
 });
