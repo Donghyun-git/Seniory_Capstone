c00a3360541c7d7a7dd7197cbbee59246dc16c7b
donghyun-git <donghuyn1113@hanmail.net>
2022-09-05 14:14:39 +0900

    21

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/* 라디오버튼 선택여부 회원가입 페이지 이동 */
+
+let manage = document.getElementById('rdo01');
+	let protect = document.getElementById('rdo02');
+	let signUp = document.querySelector('.sign-up');
+	
+	function crossPage() {
+		
+		if(manage.checked == true && protect.checked == false){
+			location.href='./link/signup.html';
+		}
+		else if(protect.checked == true && manage.checked == false){
+			location.href='./link/signup_p.html';
+		}
+	}
