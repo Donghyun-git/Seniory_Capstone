aeb55bd9fe87b75a844739558a00488a0ada0a3e
donghyun-git <donghuyn1113@hanmail.net>
2022-09-13 17:02:36 +0900

    파일수정

--- app/src/public/js/index.js
+++ app/src/public/js/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* 라디오버튼 선택여부 회원가입 페이지 이동 */
 
 let manage = document.getElementById('rdo01');
@@ -11,7 +13,7 @@ let manage = document.getElementById('rdo01');
 		}
 		else if(protect.checked == true && manage.checked == false){
 			location.href='./link/signup_p.html';
-		}
-	}
+		};
+	};
 
       
